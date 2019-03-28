cd3d69563ce5683ee1c4a6fade98139fe0f5d125
coolov <olov.sundstroem@nytimes.com>
2019-03-28 15:23:34 -0400

    include the dom-parser in the node package

--- dom.js
+++ dom.js
@@ -1,1 +1,7 @@
-console.log('hello dom');
+if (typeof window === 'undefined') {
+  throw new Error(
+    'The window object is not availible. Are you trying to run the module in NodeJS?'
+  );
+}
+
+module.exports = window.DOMParser;
--- node.js
+++ node.js
@@ -1,1 +1,10 @@
-console.log('hello node');
+if (typeof window !== 'undefined') {
+  throw new Error(
+    'The window object is availible. Are you trying to run the module in the Browser?'
+  );
+}
+
+const { JSDOM } = require('jsdom');
+const DOMParser = new JSDOM().window.DOMParser;
+
+module.exports = DOMParser;
